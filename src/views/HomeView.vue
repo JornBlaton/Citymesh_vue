b09cf06640d099b39ac93b2ca22e9635c1dd92a0
howestjorn <125253286+howestjorn@users.noreply.github.com>
2023-12-11 15:24:23 +0100

    testing date response

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,9 @@
     <div>
         <form @submit.prevent="submit" class="flex flex-col mt-3 ml-5 mr-5">
             <label for="id">Select Date:</label>
-            <input type="date" id="date" name="date">
+            <input type="date" id="date" name="date" v-model="form.date">
             <label for="amount">Amount of Participants:</label>
-            <input type="number" id="amount" name="amount">
+            <input type="number" id="amount" name="amount" v-model="form.amount">
             <save-button/>
         </form>
     </div>
@@ -12,4 +12,14 @@
 
 <script setup>
 import SaveButton from "@/components/SubmitButton.vue";
+import {reactive} from "vue";
+
+const form = reactive({
+    date: null,
+    amount: 0
+});
+
+function submit() {
+    console.log(form)
+}
 </script>
